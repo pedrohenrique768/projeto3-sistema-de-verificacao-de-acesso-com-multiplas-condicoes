6aeea6d8135b98b073bcf536eb87da9468f8cc58
Pedro Henrique <pedro.castro109@aluno.ce.gov.br>
2025-09-02 11:23:45 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+let nome = prompt("qual o seu nome?")
+let idade = prompt("qual a sua idade?")
+if(idade >= 18){
+    alert("olá ${nome}!você tem acesso completo. Boas vindas!")
+}else if (nome == "Renan"){
+    alert("Bem-Vindo, ${nome}! Você tem acesso completo. Boas vindas!")
+}
