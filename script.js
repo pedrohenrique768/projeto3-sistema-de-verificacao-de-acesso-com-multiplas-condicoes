b15007a7f7717f842696d29f162184a7b48c9a09
Pedro Henrique <pedro.castro109@aluno.ce.gov.br>
2025-09-02 11:36:26 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 let nome = prompt("qual o seu nome?")
-let idade = prompt("qual a sua idade?")
-if(idade >= "18"){
+let idade = parseInt(prompt("qual a sua idade?"))
+if(idade >= 18){
     alert(`olá ${nome}!você tem acesso completo. Boas vindas!`)
 }else if (nome == `Renan`){
     alert(`Bem-Vindo, ${nome}! Você tem acesso completo. Boas vindas!`)
 
 }
+
